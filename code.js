08c9e589415510cc314709ed26abe9fd95434b8a
ameni1212chebbi <chebbiameni1010@gmail.com>
2021-05-03 04:39:45 +1400

    array-iteration

--- code.js
+++ code.js
@@ -59,6 +59,14 @@ function sumUntil(array, index) {
     for (i=index;i>=0;i--){
     result = result + array[i]}
     return result;}
+    //Q7
+    function subtractReverse(array) { var result=array[array.length-1]
+        for( var i=array.length-2;i>=0;i--){
+        result=result-array[i]}
+        return result}
+        undefined
+        subtractReverse([1, 2, 3])
+        0
     //Q8
     function product(array) {
         var i=0
@@ -71,4 +79,122 @@ function sumUntil(array, index) {
         undefined
         product([1, 2, 3])
         6
+        //Q9
+        function average(array) {
+            var result=0
+            var average=0
+            for(i=0;i<array.length;i++){
+            result=result+array[i]
+            }
+            return average=result/array.length
+            }
+            undefined
+            average([1, 2, 3])
+            2
+            average([0, 1, 2])
+            1
+            //Q10
+            function square(array) {
+                for(i=0;i<array.length;i++){
+                array[i]=array[i]*array[i]
+                }
+                return array
+                }
+                undefined
+                square([1, 2, 3])
+                (3) [1, 4, 9]
+                //Q11
+                function isArray(array){
+                    if (toString.call(array) === "[object Array]")
+                    return true;
+                    return false;
+                    }
+                    undefined
+                    isArray([1, 2, 3])
+                    true
+                    isArray("hi")
+                    false
+                    //Q12
+                    function min(array){
+                        var m = array[0]
+                        for (var i=1;i<array.length;i++){
+                        if (array[i]<array[i+1]){
+                        m = array[i]
+                        } 
+                        if (array[i]>array[i+1]){
+                            m = array[i+1]
+                            } 
+                        }
+                     return m 
+                    }
+                    //Q13
+                    function max(array){
+                        var m = array[0]
+                        for (var i=1;i<array.length;i++){
+                        if (array[i]<array[i+1]){
+                        m = array[i]
+                        } 
+                        if (array[i]>array[i+1]){
+                            m = array[i+1]
+                            } 
+                        }
+                     return m 
+                    }
+                     //Q14
+                     function shortestString(array){
+                        var shortest=array[0].length;  
+                        for (var i=1; i<array.length;i++){
+                           if (array[i].length<shortest) {
+                               shortest=array[i].length}}
+                     return shortest; 
+                     }
+                     //Q15
+                     function longestString(array){
+                        var longest=array[0].length;  
+                        for (var i=1; i<array.length;i++){
+                           if (array[i].length>longest) {
+                               longest=array[i].length}}
+                     return longest; 
+                     }
+                     //Q16
+                     function minMax(array){
+                        var result=[];
+                        result[0]=Math.min(...array);
+                       result[1]=Math.max(...array);
+                      return result
+                    }
+                    //Q17
+                    function shortestStringOne(array){
+                        var shortest=array[0].length;  
+                        var string=array[0];
+                        for (var i=1; i<array.length;i++){
+                           if (array[i].length<shortest) {
+                               shortest=array[i].length
+                               string=array[i]}}
+                     return string; 
+                     }
+                     undefined
+                     shortestStringOne(["hi", "cat","hello"])
+                     "hi"
+                     function longestStringTwo(array){
+                        var longest=array[0].length;
+                        var string=array[0];  
+                        for (var i=1; i<array.length;i++){
+                           if (array[i].length>longest) {
+                               longest=array[i].length
+                               string=array[i]}}
+                     return string; 
+                     }
+                     undefined
+                     longestStringTwo(["hi", "cat","hello"])
+                     "hello"
+                     function shortestLongest(array){
+                         var result=[];
+                         result[0]=shortestStringOne(array);
+                         result[1]=longestStringTwo(array);
+                         return result;}
+                     undefined
+                     shortestLongest(["hi", "cat", "hello"])
+                     (2) ["hi", "hello"]
+                     
 
